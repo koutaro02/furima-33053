083470c8b1a476757c2de489d5f3b7b5fb2de26f
koutaro02 <k.u89yakyuu@icloud.com>
2021-01-09 19:18:14 +0900

    activehash導入からカテゴリーのプルダウン実装まで

--- Gemfile
+++ Gemfile
@@ -61,3 +61,6 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'devise'
 
 gem 'pry-rails'
+gem 'mini_magick'
+gem 'image_processing'
+gem 'active_hash'
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,9 @@
 class Item < ApplicationRecord
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+
+  has_one_attached :image
+
+  validates :category_id, numericality: { other_than: 1 }
 end
