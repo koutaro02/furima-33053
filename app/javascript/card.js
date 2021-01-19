beb516b4470ed5a3218c51f9ac19567fb486bf7f
koutaro02 <k.u89yakyuu@icloud.com>
2021-01-19 17:56:02 +0900

    不要な記述の削除

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -18,10 +18,7 @@ const pay = () => {
     Payjp.createToken(card,(status, response) => {
       
       if (status == 200) {
-        // console.log(status)
         const token = response.id;
-        console.log(token)
-
         const renderDom = document.getElementById("charge-form");
         const tokenObj = `<input value=${token} name='token' type="hidden">`;
         renderDom.insertAdjacentHTML("beforeend", tokenObj);
